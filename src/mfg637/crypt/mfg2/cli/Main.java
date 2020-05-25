32cddd89faa32f47e881357d78da997ca74d303d
mfg637 <mfg636@gmail.com>
2020-05-25 16:29:10 +0300

    remove outdated "mfg2fast" codec recent commits make it useless

--- src/mfg637/crypt/mfg2/cli/Main.java
+++ src/mfg637/crypt/mfg2/cli/Main.java
@@ -2,7 +2,6 @@ package mfg637.crypt.mfg2.cli;
 
 import mfg637.codec.BytesCodec;
 import mfg637.crypt.mfg2.codec.MFG2Codec;
-import mfg637.crypt.mfg2.codec.MFG2Fast;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -11,11 +10,10 @@ import java.security.NoSuchAlgorithmException;
 
 public class Main {
 
-    private enum Mode{NULL, ENCODE, DECODE, FASTDECODE, FASTENCODE}
+    private enum Mode{NULL, ENCODE, DECODE}
 
     public static void main(String[] args) {
         BytesCodec mfg2 = null;
-        BytesCodec mfg2fast = null;
         int i=0;
         Mode mode = Mode.NULL;
         while (i<args.length){
@@ -31,17 +29,10 @@ public class Main {
                     try {
                         byte[] password = args[i].getBytes(StandardCharsets.UTF_8);
                         mfg2 = new MFG2Codec(password);
-                        mfg2fast = new MFG2Fast(password);
                     } catch (NoSuchAlgorithmException e) {
                         e.printStackTrace();
                         return;
                     }
-                    break;
-                case "-fc":
-                    mode = Mode.FASTENCODE;
-                    break;
-                case "-fd":
-                    mode = Mode.FASTDECODE;
                     break;
             }
             i++;
@@ -66,34 +57,6 @@ public class Main {
                 case DECODE:
                     try {
                         output = mfg2.decode(System.in.readAllBytes());
-                    } catch (IOException | CloneNotSupportedException e) {
-                        e.printStackTrace();
-                        return;
-                    }
-                    try {
-                        System.out.write(output);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        return;
-                    }
-                    break;
-                case FASTENCODE:
-                    try {
-                        output = mfg2fast.encode(System.in.readAllBytes());
-                    } catch (DigestException | IOException | CloneNotSupportedException e) {
-                        e.printStackTrace();
-                        return;
-                    }
-                    try {
-                        System.out.write(output);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        return;
-                    }
-                    break;
-                case FASTDECODE:
-                    try {
-                        output = mfg2fast.decode(System.in.readAllBytes());
                     } catch (IOException | CloneNotSupportedException e) {
                         e.printStackTrace();
                         return;
